1ec6bf87b98a2d30018568fbc74cfd530fc1c159
Yuxuan <ychen14@stuy.edu>
2016-01-04 22:02:34 -0500

    add Sorts.java

---
+++ hwSort/Sorts.java
@@ -0,0 +1,28 @@
+public class Sorts{
+
+    public static void printArray(int[]data){
+	String message = "[";
+	if (data.length==0){
+	    message+=" ";
+	}
+	for(int i=0;i<data.length;i++){
+	    message += data[i]+",";
+	}
+	System.out.println(message.substring(0,message.length()-1)+"]");
+    }
+
+    public static void insertionSort(int[]data){
+	for(int i=1;i<data.length;i++){
+	    int carry = data[i];
+	    int x = i;
+	    while((x>0) && (data[x-1]>carry)){
+		data[x]=data[x-1];
+		data[x-1]=carry;
+		System.out.println(x);
+		x--;
+	    }
+	}
+    }
+
+
+}
