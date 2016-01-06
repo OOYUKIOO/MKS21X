51b2d3d59cc9ac25de26f6d07a8a849cc8a570c8
Yuxuan <ychen14@stuy.edu>
2016-01-05 22:30:32 -0500

    finish SelectionSort.java, test and work properly

--- hwSort/SelectionSorts.java
+++ hwSort/SelectionSorts.java
@@ -22,15 +22,31 @@ public class SelectionSorts{
 	    for(int x=start; x<end; x++){
 		min = Math.min(min,data[x]);
 	    }
+
 	    toSwap = data[start];
 	    data[start] = min;
-	    for(int x = start; x<end; x++){
-
+	    for(int x = start+1; x<end; x++){
+		if (data[x] == min){
+		    data[x] = toSwap;
+		    x = end;
+		}
 	    }
-
+	    start ++;
 	}
 
     }
 
+    //testing
+    public static void main(String[]args){
+	int[]test = {4,2,6,1,0,3};
+	printArray(test);
+	selectionSort(test);
+	printArray(test);
+
+	int[]oneMore = {3,3,1,7,34,1,6,3,10};
+	printArray(oneMore);
+	selectionSort(oneMore);
+	printArray(oneMore);
+    }
 
 }
